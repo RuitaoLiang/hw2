83f12ff761447a6b53b77ce2fc64e5f3848ac728
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-27 14:15:54 -0800

    Create Mondrian1.js

---
+++ Mondrian1.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(340, 340);
+}
+
+function draw() {
+  strokeWeight(4);
+  stroke(51);
+  line(random(width), random(height), random(width), random(height));
+  fill(255, 204, 0);
+  rect(random(width), random(height), random(width), random(height);
+  colorMode(HSB);
+  fill(255, 204, 100);
+  rect(random(width), random(height), random(width), random(height);
+  fill('red');
+  rect(random(width), random(height), random(width), random(height);
+}
+draw();
