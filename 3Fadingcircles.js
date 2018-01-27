2ead064182d72d3004a4273dba5891db2fc22882
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-27 13:52:02 -0800

    Create 3Fadingcircles.js

---
+++ 3Fadingcircles.js
@@ -0,0 +1,10 @@
+background(255);
+colorMode(HSB);
+noStroke();
+
+while(true) {
+  colorMode(HSB, 100);
+  background(0, 0, 255, 0.1);
+  fill(255, 0, 0);
+  ellipse(random(width), random(height), random(20, 40));
+}
