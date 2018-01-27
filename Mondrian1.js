16f0b85d98b0a70198f26249c58a93606ee21878
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-27 14:17:22 -0800

    Update Mondrian1.js

--- Mondrian1.js
+++ Mondrian1.js
@@ -7,11 +7,13 @@ function draw() {
   stroke(51);
   line(random(width), random(height), random(width), random(height));
   fill(255, 204, 0);
-  rect(random(width), random(height), random(width), random(height);
+  rect(random(width), random(height), random(width), random(height));
   colorMode(HSB);
   fill(255, 204, 100);
-  rect(random(width), random(height), random(width), random(height);
+  rect(random(width), random(height), random(width), random(height));
   fill('red');
-  rect(random(width), random(height), random(width), random(height);
+  rect(random(width), random(height), random(width), random(height));
+  fill(51);
+  rect(random(width), random(height), random(width), random(height));
 }
 draw();
