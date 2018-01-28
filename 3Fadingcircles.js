42fc56005ae883427f9a9f12a181cacdbe137cd9
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-28 15:13:48 -0800

    Update 3Fadingcircles.js

--- 3Fadingcircles.js
+++ 3Fadingcircles.js
@@ -1,8 +1,9 @@
 function setup() { 
-background(255);
-colorMode(HSB);
-noStroke();
-
+  background(255);
+  colorMode(HSB);
+  noStroke();
+}
+function draw(){
   colorMode(HSB, 100);
   background(0, 0, 255, 0.1);
   fill(0,100,100);
