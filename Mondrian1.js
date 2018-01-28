5af60ce3e12d8bd85828e611e64664c4017c6b8d
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-28 14:26:56 -0800

    Update Mondrian1.js

--- Mondrian1.js
+++ Mondrian1.js
@@ -1,19 +1,37 @@
 function setup() {
-  createCanvas(340, 340);
-}
-
-function draw() {
-  strokeWeight(4);
-  stroke(51);
-  line(random(width), random(height), random(width), random(height));
+  createCanvas(1200, 800);
+  strokeWeight(8);
+  stroke(80);
+  rect(1, 5, 248, 101);
   fill(255, 204, 0);
-  rect(random(width), random(height), random(width), random(height));
+  rect(249, 5, 302, 101);
   colorMode(HSB);
   fill(255, 204, 100);
-  rect(random(width), random(height), random(width), random(height));
+  rect(551, 5, 173, 192);
   fill('red');
-  rect(random(width), random(height), random(width), random(height));
+  rect(1, 105, 102, 211);
   fill(51);
-  rect(random(width), random(height), random(width), random(height));
+  rect(1, 317, 102, 325);
+	rect(1,643,551,94);
+  fill(255,255,255);
+  rect(103, 106, 449, 317);
+  colorMode(HSB);
+  fill(255, 204, 100);
+  rect(103, 423, 184, 220);
+  fill('red');
+  rect(287, 423, 121, 220);
+  rect(431, 423, 121, 220);
+  fill(255, 204, 0);
+  rect(551, 423, 173, 48);
+  colorMode(HSB);
+  fill(255, 204, 100);
+  rect(551, 471, 87, 172);
+  fill('red');
+  rect(638, 471, 87, 172);
+  fill(51);
+  rect(552, 643, 173, 94);
+	rect(1,643,551,94);
+  fill(255, 204, 0);
+  rect(103, 106, 449, 317);
 }
 
