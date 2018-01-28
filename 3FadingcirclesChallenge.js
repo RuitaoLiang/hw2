7483f570d66b7d068eb1feab6bddbb146717bc4d
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-28 14:52:11 -0800

    Update 3FadingcirclesChallenge.js

--- 3FadingcirclesChallenge.js
+++ 3FadingcirclesChallenge.js
@@ -1,11 +1,11 @@
+function setup() { 
 background(255);
 colorMode(HSB);
 noStroke();
 
-while(true) {
   colorMode(HSB, 100);
   background(0, 0, 255, 0.1);
-  fill(255, 0, 0);
+  fill(0,100,100);
   beginShape(TRIANGLE_FAN);
   vertex(57.5, 50);
   vertex(57.5, 15);
