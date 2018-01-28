e03eac70720ac93d1b41be07573ce5dfba012d53
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-28 13:32:19 -0800

    Create Mondrian2.js

---
+++ Mondrian2.js
@@ -0,0 +1,12 @@
+function setup()
+  create Canvas(600,400);
+  strokeWeight(10);
+stroke(51);
+rect(500, 300, 60, 60);
+ fill(255, 204, 0);
+rect(20, 20, 60, 60);
+colorMode(HSB);
+fill(255, 204, 100);
+rect(20, 20, 60, 60);
+fill('red');
+rect(20, 20, 60, 60);
