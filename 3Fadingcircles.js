110226aefdead14780b57bf6c7ecdc22536e2458
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-31 08:29:00 -0800

    Update 3Fadingcircles.js

--- 3Fadingcircles.js
+++ 3Fadingcircles.js
@@ -6,6 +6,6 @@ function setup() {
 function draw(){
   colorMode(HSB, 100);
   background(0, 0, 255, 0.1);
-  fill(0,100,100);
+  fill(random(150,200),255,255);
   ellipse(random(width), random(height), random(20, 40));
 }
